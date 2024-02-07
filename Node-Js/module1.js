bcad76d99f439ad2f6dfac4d9f158600beac6960
Working Station <tn8070250@gmail.com>
2024-02-07 17:57:39 +0630

    Ch-1 Ep-4 Node Module System

---
+++ Node-Js/module1.js
@@ -0,0 +1,9 @@
+const myname = 'thu ta nyan';
+const myage = 18;
+const hair = 'black';
+
+module.exports = {
+    myname,
+    myage,
+    hair
+}
---
+++ Node-Js/module2.js
@@ -0,0 +1,5 @@
+const {myname,myage,hair} = require('./module1');
+
+console.log('Name:', myname);
+console.log('Age:', myage);
+console.log('Hair color:', hair);
