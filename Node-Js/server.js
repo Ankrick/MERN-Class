92211dade362a1261f755ba00427a88718db5c38
Thu Ta Nyan <tn8070250>
2024-02-07 23:14:42 +0630

    Ch-2 Ep-11 Redirect URL

--- Node-Js/server.js
+++ Node-Js/server.js
@@ -16,6 +16,10 @@ const server = http.createServer((req, res) => {
       filename = "contact.html";
       res.statusCode = 200;
       break;
+    case "/contact-us":
+      res.statusCode = 301;
+      res.setHeader("Location", "/contact");
+      break;
     default:
       filename = "404.html";
       res.statusCode = 404;
