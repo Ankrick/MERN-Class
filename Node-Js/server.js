8dac31aeb037db45954d6e48456f51560150da40
Thu Ta Nyan <tn8070250>
2024-02-07 23:09:06 +0630

    Ch-2 Ep-10 Http Status Codes

--- Node-Js/server.js
+++ Node-Js/server.js
@@ -4,17 +4,21 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
   let filename;
   switch (req.url) {
-    case "/home":
+    case "/":
       filename = "home.html";
+      res.statusCode = 200;
       break;
     case "/about":
       filename = "about.html";
+      res.statusCode = 200;
       break;
     case "/contact":
       filename = "contact.html";
+      res.statusCode = 200;
       break;
-    case "/404":
+    default:
       filename = "404.html";
+      res.statusCode = 404;
       break;
   }
 
