5339f1bdf3edb806c9f44b80b29ffb6a2ee4556e
Thu Ta Nyan <tn8070250>
2024-02-08 22:51:41 +0630

    Ch-3 Ep-19 Dynamic title

--- Node-Js/app.js
+++ Node-Js/app.js
@@ -6,34 +6,42 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   let blogs = [
-    // { title: "Blog title update 1", intro: "this is blog intro 1" },
-    // { title: "Blog title 2", intro: "this is blog intro 2" },
-    // { title: "Blog title 3", intro: "this is blog intro 3" },
+    { title: "Blog title update 1", intro: "this is blog intro 1" },
+    { title: "Blog title 2", intro: "this is blog intro 2" },
+    { title: "Blog title 3", intro: "this is blog intro 3" },
   ];
 
   res.render("home", {
     blogs,
+    title: "Home"
   });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
-});
+  res.render("about", {
+    title: "About"
+  });
+})
 
 app.get("/about-us", (req, res) => {
   res.redirect("/about");
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact");
+  res.render("contact", {
+    title: "Contact"
+  });
 });
 
 app.get("/contact-us", (req, res) => {
-  res.redirect("/contact");
+  res.redirect("/contact", {
+    title: "Contact"
+  });
 });
 
+
 app.use((req, res) => {
-  res.render("404");
+  res.render("404", {title: "404"});
   res.status(404);
 });
 
