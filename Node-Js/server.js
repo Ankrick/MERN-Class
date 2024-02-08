9688ddcf26619f1764ba290d3b025ff1d8114f77
Thu Ta Nyan <tn8070250>
2024-02-08 09:07:00 +0630

    Ch-3 Ep-13 install Project Specified NPM packages

--- Node-Js/server.js
+++ Node-Js/server.js
@@ -1,10 +1,14 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require("lodash");
+
+console.log(_.random(20));
 
 const server = http.createServer((req, res) => {
   let filename;
   switch (req.url) {
     case "/":
+      console.log("home page is requesting");
       filename = "home.html";
       res.statusCode = 200;
       break;
