a47f310bffcbaa435bd1fa403a09acba26880648
Thu Ta Nyan <tn8070250>
2024-02-19 13:31:51 +0630

    Ch-5 Ep-41 to Ep -45

---
+++ react-basic/my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import logo from './logo192.png'
+
+function App() {
+  let name = "thu ta nyan"
+
+  return (
+    <div className="app">
+      <h1>Hello {name}</h1>
+    </div>
+  );
+}
+
+export default App;
