06a2e6fe2b60380f85b70192ae211ba71d0b3d71
Thu Ta Nyan <tn8070250>
2024-02-19 15:21:35 +0630

    Ch-5 Ep-48 to Ep-49

--- react-basic/my-app/src/App.js
+++ react-basic/my-app/src/App.js
@@ -5,6 +5,20 @@ import { useState } from 'react';
 function App() {
   // let name = "thu ta nyan"
   let [name, setName] = useState('thu ta nyan');
+  let [posts, setPost] = useState([
+    {
+      id : 1,
+      title : 'First Post'
+    },
+    {
+      id : 2,
+      title : 'Second Post'
+    },
+    {
+      id : 3,
+      title : 'Third Post'
+    }
+  ]);
 
   let changeName = () => {
     setName('aung aung');
@@ -15,6 +29,13 @@ function App() {
     <div className="app">
       <h1>Hello {name}</h1>
       <button onClick={changeName}>change name</button>
+
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post)=>(
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
