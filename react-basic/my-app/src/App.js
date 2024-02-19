1fbe7eebeb8b6ce0cce8cc860f497e4a45619852
Thu Ta Nyan <tn8070250>
2024-02-19 13:57:25 +0630

    Ch-5 Ep-42 to Ep-47

--- react-basic/my-app/src/App.js
+++ react-basic/my-app/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import logo from './logo192.png'
+import { useState } from 'react';
 
 function App() {
-  let name = "thu ta nyan"
+  // let name = "thu ta nyan"
+  let [name, setName] = useState('thu ta nyan');
+
+  let changeName = () => {
+    setName('aung aung');
+    console.log(name);
+  }
 
   return (
     <div className="app">
       <h1>Hello {name}</h1>
+      <button onClick={changeName}>change name</button>
     </div>
   );
 }
