76fd236182c06792cac49c01497b3d391db9e96b
Thu Ta Nyan <tn8070250@gmail.com>
2025-03-11 00:17:33 +0630

    Ep-125 to Ep-126

--- Recipes/frontend/src/main.jsx
+++ Recipes/frontend/src/main.jsx
@@ -1,57 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import About from './pages/About.jsx'
-import Contact from './pages/Contact.jsx'
-import RecipeForm from './pages/RecipeForm.jsx'
-import SignUpForm from './pages/SignUpForm.jsx'
-import SignInForm from './pages/SignInForm.jsx'
+import Routes from './routes/index.jsx'
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
 
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './pages/Home.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    children: [
-      {
-        path: "",
-        element: <Home/>
-      },
-      {
-        path: "/about",
-        element: <About/>
-      },
-      {
-        path: "/contact",
-        element: <Contact/>
-      },
-      {
-        path: "/recipes/create",
-        element: <RecipeForm/>
-      },
-      {
-        path: "/recipes/edit/:id",
-        element: <RecipeForm/>
-      },
-      {
-        path: "/sign-up",
-        element: <SignUpForm/>
-      },
-      {
-        path: "/sign-in",
-        element: <SignInForm/>
-      }
-    ]
-  },
-]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AuthContextProvider><RouterProvider router={router} /></AuthContextProvider>
+  <AuthContextProvider><Routes/></AuthContextProvider>
 )
